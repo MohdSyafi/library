3018a5a7eec9e6b6efc07ae16481ff0946fd3e30
syafi <syafimohamad13@gmail.com>
2022-09-18 20:46:50 +0800

    Add functions to update book status

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
-let myLibrary = [new Book('naruto','skfdlsfjslfj','tite kubo',5,'read')];
+let myLibrary = 
+[
+    new Book
+    (
+        'Monster Hunter',
+        'You hunt monster. You wear their skins. Rinse and repeat. The coolest weapon in the game is the boomstick. You hold a lance that can shoot like a gun. Isnt that awesome.',
+        'tite kubo',
+        5,
+        'read'
+    )
+    ,new Book
+    (
+        'Nier Automata',
+        'You are an android in a dystopian world fighting murder hobo robots thingy majiggy. There is a lot of metaphors that normal wet brain people might find hard to understand.',
+        'Yoko Taro',
+        12,
+        'unread'
+    )
+];
 
 function Book(title,summary,author,pages,status) {
   this.title = title,
   this.summary = summary,
   this.author = author,
   this.pages = pages,
-  this.status = status
+  this.status = status,
+  Book.prototype.updateStatus = function() {
+    if(this.status==="read")
+        this.status = "unread"
+    else
+        this.status = "read"
+  }
 }
 
 function closeDialog(){
     addDialog.style.display = 'none'
+    addDialogContainer.style.display = 'none'
 }
 
 function openDialog(){
-    if(addDialog.style.display === 'block')
+    if(addDialog.style.display === 'block'){
         closeDialog();
-    else
-        addDialog.style.display = 'block'
+    }     
+    else{
+        addDialog.style.display = 'block';
+        addDialogContainer.style.display = 'block';
+    }
+        
 }
 
 function addBookToLibrary(form) {
@@ -28,27 +57,58 @@ function addBookToLibrary(form) {
     form.elements['form-status'].value
    )
    var length = myLibrary.push(newBook);
-   updateLibrary(length-1);
+   appendLibrary(length-1);
 }
 
- function updateLibrary(newIndex){
+ function appendLibrary(newIndex){
 
+    const newBook = myLibrary[newIndex];
+    const newNode = createNode(newBook,newIndex);
+    let content = document.querySelector('.content');
+    content.appendChild(newNode);
+ }
+
+ function loadLibrary(){
+
+    let content = document.querySelector('.content');
+
+    if(myLibrary.length>0){    
+        myLibrary.forEach((book,index)=>{
+           const newNode = createNode(book,index);
+           content.appendChild(newNode);
+        });
+    }
+ }
+
+ function createNode(book,bookIndex){
     let card = document.querySelector('.cards:nth-child(2)');
     const clone = card.cloneNode(true);
-    const newBook = myLibrary[newIndex];
+    const newId = "card" + `${bookIndex}`;
+    clone.setAttribute('id',newId);
+    clone.querySelector(`.bookTitle`).textContent = book.title;
+    clone.querySelector(`.summary`).textContent = book.summary;
+    clone.querySelector(`.author`).textContent = book.author;
+    clone.querySelector(`.pages`).textContent = book.pages;
+    clone.querySelector(`.status`).textContent = book.status;
+    clone.querySelector(`.updateBtn`).setAttribute('data-index',bookIndex);
+    clone.querySelector(`.updateBtn`).setAttribute('id',"updateBtn" + `${bookIndex}`);
+    clone.querySelector(`.removeBtn`).setAttribute('data-index',bookIndex);
+    clone.querySelector(`.removeBtn`).setAttribute('id',"removeBtn" + `${bookIndex}`);
+    clone.setAttribute(`data-index`,bookIndex);
 
-    clone.querySelector("#title").textContent = newBook.title;
-    clone.querySelector("#summary").textContent = newBook.summary;
-    clone.querySelector("#author").textContent = newBook.author;
-    clone.querySelector("#pages").textContent = newBook.pages;
-    clone.querySelector("#status").textContent = newBook.status;
-    
-    let content = document.querySelector('.content');
-    content.appendChild(clone);
+    return clone;
+ }
+
+ function updateBook(bookIndex){
+    myLibrary[bookIndex].updateStatus();
+    const updatedCard = document.querySelector(`div.cards[data-index="${bookIndex}"]`);
+    updatedCard.querySelector(`.status`).textContent = myLibrary[bookIndex].status;
  }
 
 const addDialog = document.querySelector(".add-book-dialog");
-addDialog.style.display = 'none';
+const addDialogContainer = document.querySelector(".dialogContainer");
+
+closeDialog();
 
 const newButton = document.querySelector("div.add-cards");
 newButton.addEventListener('click',openDialog);
@@ -61,7 +121,11 @@ const submit = document.querySelector("form");
 submit.addEventListener('submit', e => {
     e.preventDefault();
     addBookToLibrary(submit);
+    closeDialog();
 });
 
+loadLibrary();
 
+const updateButtons = document.querySelectorAll(".updateBtn");
+updateButtons.forEach(item=>item.addEventListener('click',(e)=> updateBook(e.target.getAttribute('data-index'))));
 
